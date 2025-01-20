bbf268fd0866254c59219f38d3e65b6577cbf702
Daniel Ocampo <danielocampo182@gmail.com>
2025-01-19 20:25:26 -0600

    feat(home): display basic dog api results

---
+++ src/lib/components/homePage/DogCard.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	import type { Dog } from '$lib/api/dogs/models';
+	import * as Card from '$lib/components/ui/card/index';
+
+	let { dogData }: { dogData: Dog } = $props();
+</script>
+
+<Card.Root>
+	<Card.Content class="h-72 w-full">
+		<img class="h-full w-full rounded-t-lg object-cover" src={dogData.img} alt={dogData.name} />
+	</Card.Content>
+	<Card.Footer class="">
+		<p class="p-2 text-center text-2xl font-bold leading-7">{dogData.name}</p>
+		<div class="flex flex-col gap-2">
+			<p class="leading-7"><span class="font-bold">Breed:</span> {dogData.breed}</p>
+			<p class="leading-7"><span class="font-bold">Zip Code:</span> {dogData.zip_code}</p>
+		</div>
+	</Card.Footer>
+</Card.Root>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import type { Dog } from '$lib/api/dogs/models';
 	import { getDogs } from '$lib/api/dogs/utils.svelte';
+	import DogCard from '$lib/components/homePage/DogCard.svelte';
 	import { onMount } from 'svelte';
 
 	let dogs = $state<Dog[]>([]);
@@ -9,3 +10,15 @@
 		dogs = await getDogs();
 	});
 </script>
+
+<header>
+	<h1 class="scroll-m-20 p-2 text-4xl font-extrabold tracking-tight lg:text-5xl">
+		Fetch Pet Finder
+	</h1>
+</header>
+
+<main class="grid grid-cols-1 gap-5 p-16 md:grid-cols-3">
+	{#each dogs as dogData}
+		<DogCard {dogData} />
+	{/each}
+</main>
