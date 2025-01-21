542a556cbfdbe56e23957d00fdd678a30e202c5f
Daniel Ocampo <danielocampo182@gmail.com>
2025-01-21 07:45:41 -0600

    feat(filtering-component): add rest of filtering fields

--- src/lib/components/homePage/filtering/FilteringComponent.svelte
+++ src/lib/components/homePage/filtering/FilteringComponent.svelte
@@ -2,6 +2,9 @@
 	import { onMount } from 'svelte';
 	import BreedSearchComboBox from './BreedSearchComboBox.svelte';
 	import { DogBreedsHandler } from './state/DogBreedState.svelte';
+	import { Label } from '$lib/components/ui/label';
+	import { Input } from '$lib/components/ui/input';
+	import type { FilterState } from './state/FilterQueryState.svelte';
 
 	const dogBreeds = new DogBreedsHandler();
 
@@ -9,9 +12,32 @@
 		dogBreeds.fetchAllDogBreeds();
 	});
 
-	let { filterState = $bindable() } = $props();
+	let { filterState = $bindable() }: { filterState: FilterState } = $props();
 </script>
 
-<aside class="flex w-full flex-col items-center lg:max-w-64">
+<aside class="flex w-full flex-col items-center gap-5 lg:max-w-64">
 	<BreedSearchComboBox allDogBreeds={dogBreeds.data} {filterState} />
+
+	<section class="w-full text-center">
+		<p
+			class="py-2 text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+		>
+			Age Range
+		</p>
+
+		<div class="flex items-center gap-2">
+			<Label for="min-age" class="visually-hidden">Minimum Age</Label>
+			<Input id="min-age" bind:value={filterState.ageMin} type="number" />
+
+			<span>to</span>
+
+			<Label for="max-age" class="visually-hidden">Maximum Age</Label>
+			<Input id="max-age" bind:value={filterState.ageMax} type="number" />
+		</div>
+	</section>
+
+	<section class="flex w-full flex-col items-center gap-2">
+		<Label for="zip-codes">Zip Codes</Label>
+		<Input id="zip-codes" bind:value={filterState.zipCodes}></Input>
+	</section>
 </aside>
