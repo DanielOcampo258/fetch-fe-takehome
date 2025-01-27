920208d2e5fcf75e613a0bbff5cf1bdc9fa2d397
Daniel Ocampo <danielocampo182@gmail.com>
2025-01-27 11:02:28 -0600

    feat(dog-search): add basic loading spinner

--- src/lib/components/DogSearch/DogSearch.svelte
+++ src/lib/components/DogSearch/DogSearch.svelte
@@ -9,6 +9,7 @@
 	import { DogSearchState } from './state/DogSearchState.svelte';
 	import { untrack } from 'svelte';
 	import DogCard from '../DogCard/DogCard.svelte';
+	import LoadingSpinner from '../ui/LoadingSpinner/LoadingSpinner.svelte';
 
 	let { favoritesState, fetchDogMatch } = $props();
 
@@ -32,7 +33,7 @@
 	});
 </script>
 
-<main class="mb-5 bg-[#f1f0f2]">
+<main class="mb-5 bg-[#f1f0f2] {dogSearchState.isLoading ? 'cursor-wait' : 'cursor-auto'}">
 	<section class="flex flex-col items-center gap-5 p-12 md:flex-row md:items-start">
 		<aside class="top-10 flex flex-col gap-5 md:sticky md:max-w-xs">
 			<FilteringComponent {filterState} />
@@ -45,23 +46,27 @@
 			>
 		</aside>
 
-		<section class="grid grid-cols-1 gap-5 lg:grid-cols-2 xl:grid-cols-3">
-			{#each dogSearchState.dogs as dogData (dogData.id)}
-				<DogCard {dogData}>
-					{#snippet cardAction(dog: Dog)}
-						<Button
-							aria-label={favoritesState.getAriaLabel(dog)}
-							aria-pressed={favoritesState.isFavorited(dog.id)}
-							size="icon"
-							variant="ghost"
-							onclick={() => favoritesState.toggleFavoriteState(dog.id)}
-						>
-							<Heart fill={favoritesState.isFavorited(dogData.id) ? '#000' : 'none'} />
-						</Button>
-					{/snippet}
-				</DogCard>
-			{/each}
-		</section>
+		{#if dogSearchState.isLoading}
+			<LoadingSpinner />
+		{:else}
+			<section class="grid grid-cols-1 gap-5 lg:grid-cols-2 xl:grid-cols-3">
+				{#each dogSearchState.dogs as dogData (dogData.id)}
+					<DogCard {dogData}>
+						{#snippet cardAction(dog: Dog)}
+							<Button
+								aria-label={favoritesState.getAriaLabel(dog)}
+								aria-pressed={favoritesState.isFavorited(dog.id)}
+								size="icon"
+								variant="ghost"
+								onclick={() => favoritesState.toggleFavoriteState(dog.id)}
+							>
+								<Heart fill={favoritesState.isFavorited(dogData.id) ? '#000' : 'none'} />
+							</Button>
+						{/snippet}
+					</DogCard>
+				{/each}
+			</section>
+		{/if}
 	</section>
 </main>
 
---
+++ src/lib/components/ui/LoadingSpinner/LoadingSpinner.svelte
@@ -0,0 +1,6 @@
+<!-- Source: https://www.w3schools.com/howto/howto_css_loader.asp -->
+<div
+	role="status"
+	aria-live="polite"
+	class="mx-auto h-[100px] w-[100px] animate-spin rounded-[50%] border-[16px] border-t-[16px] border-accent border-t-primary"
+></div>
